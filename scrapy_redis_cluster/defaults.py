11088f13558dc48d591b717050331a28aed4b35d
lijiaqi <lijiaqi@xiaobing.ai>
2022-04-21 17:56:56 +0800

    fix: change default queue.

--- scrapy_redis_cluster/defaults.py
+++ scrapy_redis_cluster/defaults.py
@@ -21,9 +21,9 @@ REDIS_PARAMS = {
 }
 
 SCHEDULER_QUEUE_KEY = '%(spider)s:requests'
-SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.PriorityQueue'
+SCHEDULER_QUEUE_CLASS = 'scrapy_redis_cluster.queue.PriorityQueue'
 SCHEDULER_DUPEFILTER_KEY = '%(spider)s:dupefilter'
-SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'
+SCHEDULER_DUPEFILTER_CLASS = 'scrapy_redis_cluster.dupefilter.RFPDupeFilter'
 SCHEDULER_PERSIST = False
 START_URLS_KEY = '%(name)s:start_urls'
 START_URLS_AS_SET = False
